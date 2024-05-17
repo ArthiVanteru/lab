2cb60395f49f9c2608aa808ba35217ed1e9bd1ac
ArthiVanteru <168722660+ArthiVanteru@users.noreply.github.com>
2024-05-17 12:25:23 +0530

    DataType.java

---
+++ DataType.java
@@ -0,0 +1,23 @@
+class DataType
+{
+  public static void main(String args[])
+  {
+    int i=34;
+    char c='A';
+    byte b=4;
+    short s=54;
+    
+    double d=23.5637893;
+    float f=3.573883f;
+    long l= 26738288;
+    String st="AnudipClasses";
+    System.out.println("char: " + c);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+        System.out.println("string:"+st);
+  }
+}
